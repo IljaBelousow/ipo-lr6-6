e38948258e2d6b410dbacba99d1d2f992d51834a
ПИКМИ МЕН <103372208+IljaBelousow@users.noreply.github.com>
2024-12-06 10:29:26 +0300

    Update ipo-lr6-6.py

--- ipo-lr6-6.py
+++ ipo-lr6-6.py
@@ -2,7 +2,7 @@ from random import randint
 index = 0
 comb = 0
 list = []
-main_input = int(input("vvedite chislo: "))
+main_input = int(input("введите число: "))
 num_list = [[randint(-10, 10) for i in range(4)] for i in range(20)]#создает список из 20 подсписок в 4 чсила от -10 до 10
 for i in range(len(num_list)):#i по длине списка
     for j in range(i, len(num_list)):#j по длине списка
@@ -12,5 +12,5 @@ for i in range(len(num_list)):#i по длине списка
             index += 1
 print(list)
 print("-----------------------------")
-print("kolvo combination < inputa: ", index)
-print("kolvo combination", comb)
+print("ковло комбинций < введёного числа: ", index)
+print("колво комбинаций: ", comb)
